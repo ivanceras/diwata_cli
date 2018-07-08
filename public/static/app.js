055b150cbe15057c243c0d99af0fbed3c7e89626
Jovansonlee Cesar <ivanceras@gmail.com>
2018-07-09 05:05:19 +0800

    Regenerated, modified run script default port

--- public/static/app.js
+++ public/static/app.js
@@ -1,15 +1,48 @@
 // the db_url and grouped is retrieved from the indexed db
 function init(){
+    var username = null;
+    var password = null;
+    if (window.localStorage){
+        username = localStorage.getItem('username');
+        password = localStorage.getItem('password');
+        console.log("username:", username);
+        console.log("password:", password);
+    }
+    else{
+        console.error("localStorage is not supported");
+    }
+    var cred = null;
+    if (username != null && password != null) {
+        cred = {username: username, password: password}
+    }
     app = Elm.Main.fullscreen(
-        { db_url: null,
+        { login_required: false,
           db_name: null,
           api_endpoint: null,
           grouped: true,
+          cred: cred 
         }
     );
 
     app.ports.title.subscribe(function(title) {
         document.title = title;
+    });
+
+    app.ports.setUsername.subscribe(function(username) {
+        if (window.localStorage){
+            localStorage.setItem('username', username);
+        }
+        else{
+            console.error("localStorage is not supported");
+        }
+    });
+    app.ports.setPassword.subscribe(function(password) {
+        if (window.localStorage){
+            localStorage.setItem('password', password);
+        }
+        else{
+            console.error("localStorage is not supported");
+        }
     });
 }
 window.onload = init
--- run_sakila.sh
+++ run_sakila.sh
@@ -1,1 +1,1 @@
-cargo run -- --db-url=postgres://postgres:p0stgr3s@localhost:5432/sakila
+cargo run -- --db-url=postgres://postgres:p0stgr3s@localhost:5432/sakila -p 8001 -o
